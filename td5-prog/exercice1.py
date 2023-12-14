1bf2b7041687cc704b4187f744a4bda586653a59
EWEN GADONNAUD <egadonna@salle1>
2023-12-14 13:21:37 +0100

    nouveau dépot, premier exo du td5

---
+++ td5-prog/exercice1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 14 13:07:59 2023
+
+@author: egadonna
+"""
+
+ma_liste = [5.4, 6.3, 1.2, 5.4, -1.2, -8.5]
+
+print("Valeur la plus petite",min(ma_liste))
+print("Valeur la plus grande : ", max(ma_liste))
+print("Somme totale : ",sum(ma_liste))
+print("Liste triée à l'envers : " , sorted(ma_liste,reverse=True))
